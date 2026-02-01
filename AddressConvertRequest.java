1bd82d4c12c48a2196b86c03fffba3d10e763350
awnWatanyan <awn_watanyan@aeon.co.th>
2026-02-01 23:41:35 +0700

    Add files via upload

---
+++ AddressConvertRequest.java
@@ -0,0 +1,20 @@
+package com.aeon.acss.fdu.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class AddressConvertRequest {
+    private String province;
+    private String district;
+    private String subDistrict;
+    private String zipcode;
+	private String address;
+}
